27115402894e4964cd33a40435773e9dfc178a6c
Jacke1012 <46431999+Jacke1012@users.noreply.github.com>
2020-10-18 14:23:58 +0200

    Before school

--- Övning1/Form1.cs
+++ Övning1/Form1.cs
@@ -4,62 +4,57 @@ using System.Data.SqlClient;
 using System.Windows.Forms;
 using System.Collections.Generic;
 using System.Linq;
+using Parser;
 
 namespace Övning1
 {
     public partial class Form1 : Form
     {
-        string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\jacjon0310\source\repos\Object Orianterat(1)\Övning1\Resor.mdf;Integrated Security=True";
+        string skolConnection = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\jacjon0310\source\repos\Object Orianterat(1)\Övning1\Resor.mdf;Integrated Security=True";
+
+        string hemConnection = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\Projects Second Drive\School Shit\Object Orianterat\Övning1\Resor.mdf;Integrated Security=True";
+
+        string connectionString;
 
         List<Resa> resor = new List<Resa>();
 
         public Form1()
         {
             InitializeComponent();
+            connectionString = hemConnection;
         }
 
-        
+
 
         private void BtnSök_Click(object sender, EventArgs e)
         {
             lbxResor.Items.Clear();
             lbxResor.Items.AddRange(resor.Where(x => x.namn == tbxNamn.Text).ToArray());
-
-
-            //lbxResor.Items.Clear();
-            //string query = $"SELECT Destination, AntalDagar FROM Resor WHERE Namn ='{tbxNamn.Text}'";
-            //using (SqlConnection connection = new SqlConnection(connectionString))
-            //{
-            //    connection.Open();
-            //    using (SqlCommand cmd = new SqlCommand(query, connection))
-            //    {
-            //        using (SqlDataReader reader = cmd.ExecuteReader())
-            //        {
-            //            while (reader.Read())
-            //            {
-            //                lbxResor.Items.Add($"{reader.GetString(0).Trim()}, {reader.GetInt32(1)}");
-            //            }
-            //        }
-            //    }
-            //}
         }
 
         private void BtnBoka_Click(object sender, EventArgs e)
         {
-            string query = $"INSERT Resor(Namn, Destination, AntalDagar) VALUES('{tbxKund.Text}','{tbxDestination.Text}','{tbxAntalDagar.Text}')";
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            double temp;
+            if (TryParse.Double(tbxAntalDagar, out temp))
             {
-                connection.Open();
-                using(SqlCommand command = new SqlCommand(query, connection))
+                string query = $"INSERT Resor(Namn, Destination, AntalDagar) VALUES('{tbxKund.Text}','{tbxDestination.Text}','{temp}')";
+                using (SqlConnection connection = new SqlConnection(connectionString))
                 {
-                    command.CommandType = CommandType.Text;
-                    command.ExecuteNonQuery();
-                };
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.CommandType = CommandType.Text;
+                        command.ExecuteNonQuery();
+                    };
+                }
+
+                MessageBox.Show("Resand är bokad", "Bekräftelse", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                FetchFromDB();
+
+                tbxKund.Clear(); ;
+                tbxDestination.Clear();
+                tbxAntalDagar.Clear();
             }
-
-            MessageBox.Show("Resand är bokad", "Bekräftelse", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            FetchFromDB();
-
         }
 
         private void Form1_Load(object sender, EventArgs e)
